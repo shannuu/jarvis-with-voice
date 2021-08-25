0c09b088d29978c6e7b03a0f1dce51e5fa688686
Kushal <64453862+shannuu@users.noreply.github.com>
2021-08-25 13:53:49 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -4,6 +4,10 @@ import wikipedia
 import webbrowser
 import os
 
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
 def speak(audio, torf):
     if torf == 0:
         print(audio)
